02779d7e1caa743028283828fb7ea395c57b8e0e
congut <clcongut@outlook.com>
2024-09-15 12:11:41 +0800

    完成i2s_93库测试使用，并且更新一个小版本

--- esp32_msm261_test/include/I2S_93.h
+++ esp32_msm261_test/include/I2S_93.h
@@ -3,10 +3,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-namespace esp_i2s
-{
 #include "driver/i2s.h"
-}
 
 class I2S_93
 {
@@ -39,7 +36,6 @@ public:
     MAX,              /*!< standard max*/
   } i2scommformat_t;
 
-  
   /**
    * @brief Construct a new i2s_93 objects. Setting use which i2s and i2s mode.
    *
@@ -59,8 +55,8 @@ public:
   /**
    * @brief Set two of the formates.
    *
-   * @param channelFormate View i2schnformate_t enum
-   * @param commonFormate View i2scommformat_t enum
+   * @param channelFormate View i2schnformate_t enum.
+   * @param commonFormate View i2scommformat_t enum.
    */
   void setFormate(i2schnformate_t channelFormate, i2scommformat_t commonFormate);
   /**
@@ -70,69 +66,60 @@ public:
    */
   void setIntrAllocFlags(uint8_t intrAlloc);
   /**
-   * @brief Set the I2S DMA buffer
+   * @brief Set the I2S DMA buffer.
    *
-   * @param dmaBufCnt
-   * @param dmaBufLen
+   * @param dmaBufCnt DMA buffer count.
+   * @param dmaBufLen DMA buffer lengh.
    */
   void setDMABuffer(int dmaBufCnt, int dmaBufLen);
   /**
-   * @brief Install i2s with PCM mode
-   * 
-   * @param bckPin 
-   * @param wsPin 
-   * @param dataPin 
+   * @brief Install i2s with PCM mode.
+   *
+   * @param bckPin
+   * @param wsPin
+   * @param dataPin
    */
   void install(int bckPin, int wsPin, int dataPin);
   /**
-   * @brief Install i2s with PDM mode
+   * @brief Install i2s with PDM mode.
    *
    * @param bckPin
    * @param dataPin
    */
   void install(int bckPin, int dataPin);
-
+  /**
+   * @brief Read i2s device's data into ram.
+   *
+   * @param storageAddr The address of ram to set sample data.
+   * @param sampleSize The ram size, will caculate from bps.
+   * @return size_t
+   */
   size_t Read(void *storageAddr, int sampleSize);
-
+  /**
+   * @brief Write rame's data into i2s devic.
+   *
+   * @param storageAddr The address of ram to set sample data.
+   * @param sampleSize The ram size, will caculate from bps.
+   * @return size_t
+   */
   size_t Write(void *storageAddr, int sampleSize);
-
+  /**
+   * @brief Stop the i2s.
+   *
+   */
   void End();
 
-#if 0
-  void SetSampleRate(uint32_t sampleRate);
-  void SetChannelFormat(ChannelFormat format);
-  void SetDMABuffer(int cnt, int len);
-  void SetIntrAllocFlags(uint8_t interrupt);
-  void SetTxAutoClear(bool auto_cl_tx);
-  bool SetInputMode(int bckPin, int wsPin, int dinPin);
-  bool SetOutputMode(int bckPin, int wsPin, int douPin);
-  bool SetInOutputMode(int bckPin, int wsPin, int dinPin, int douPin);
-
-#endif
-
 private:
-  esp_i2s::i2s_port_t _deviceIndex;
-  esp_i2s::i2s_mode_t _i2sdvsMode;
+  i2s_port_t _deviceIndex;
+  i2s_mode_t _i2sdvsMode;
   i2smode_t _transmitMode;
   i2smode_t _modulateMode;
   uint32_t _sampleRate;
-  esp_i2s::i2s_bits_per_sample_t _bitsPerSample;
-  esp_i2s::i2s_channel_fmt_t _channelFormate;
-  esp_i2s::i2s_comm_format_t _commonFormate;
+  i2s_bits_per_sample_t _bitsPerSample;
+  i2s_channel_fmt_t _channelFormate;
+  i2s_comm_format_t _commonFormate;
   uint8_t _intrAlloc;
   int _dmaBufCnt;
   int _dmaBufLen;
   bool _useApll;
-
-#if 0
-  esp_i2s::i2s_port_t _i2s_port;
-  esp_i2s::i2s_bits_per_sample_t _BPS;
-  uint32_t _sampleRate = 44100;
-  bool _mode_set = true;
-  esp_i2s::i2s_channel_fmt_t _format = esp_i2s::I2S_CHANNEL_FMT_RIGHT_LEFT;
-  int _buffer_cnt = 16;
-  int _buffer_len = 64;
-  uint8_t _interrupt = 0;
-  bool _auto_cl_tx = false;
-#endif
 };
